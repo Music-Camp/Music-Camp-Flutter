b057461becbe06f6f96fb53fe763358929b0c46b
ayz1070 <ayz1070@gmail.com>
2024-05-29 11:32:11 +0900

    [feat] 바텀 네비게이션 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:music_camp/presentation/main/provider/tab_page_index_provider.dart';
-import 'package:music_camp/presentation/screens/home/home_screen.dart';
+import 'package:music_camp/presentation/screens/main/s_main.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-import 'package:music_camp/presentation/main/component/main_bottom_navigation_bar.dart';
-import 'package:provider/provider.dart';
 
-void main() {
+
+void main() async{
+  final bindings = WidgetsFlutterBinding.ensureInitialized();
+  // 네이티브 화면을 보존해줌
+  // main에서 지워주는 코드를 작성해야함
+  FlutterNativeSplash.preserve(widgetsBinding: bindings);
+
   runApp(MusicCampApp());
 }
 
@@ -20,20 +25,14 @@ class _MusicCampAppState extends State<MusicCampApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'MusicCamp',
-        theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-                seedColor: Colors.blue,
-                // 전체적인 어플의 테마를 어두운 테마로 설정한다.
-                brightness: Brightness.dark),
-            useMaterial3: true),
-
-        home: ChangeNotifierProvider(
-          create: (BuildContext context) => TabPageIndexProvider(),
-          child: Scaffold(
-            bottomNavigationBar: MainBottomNavigationBar(),
-            body: MainScreen(),
-          ),
-        ));
+      title: 'MusicCamp',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.blue,
+              // 전체적인 어플의 테마를 어두운 테마로 설정한다.
+              brightness: Brightness.dark),
+          useMaterial3: true),
+      home: const MainScreen(),
+    );
   }
 }
---
+++ lib/presentation/screens/main/tab/tab_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:music_camp/presentation/screens/attend/attend_screen.dart';
+import 'package:music_camp/presentation/screens/board/board_screen.dart';
+import 'package:music_camp/presentation/screens/login/login_main_screen.dart';
+import 'package:music_camp/presentation/screens/user/user_screen.dart';
+
+enum TabItem {
+  board(Icons.book, '게시판', BoardScreen()),
+  attend(Icons.check_circle, '출석', AttendScreen()),
+  user(Icons.person, '유저', UserScreen()),
+  login(Icons.login, '로그인', LoginMainScreen());
+
+  final IconData activeIcon;
+  final IconData inActiveIcon;
+  final String tabName;
+  final Widget firstPage;
+
+  const TabItem(this.activeIcon, this.tabName, this.firstPage, {IconData? inActiveIcon})
+      : inActiveIcon = inActiveIcon ?? activeIcon;
+
+  BottomNavigationBarItem toNavigationBarItem(BuildContext context, {required bool isActivated}) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          key: ValueKey(tabName),
+          isActivated ? activeIcon : inActiveIcon,
+          // color: isActivated ? context.appColors.iconButton : context.appColors.iconButtonInactivate,
+        ),
+        label: tabName);
+  }
+}
+
